ef12a4a4f5b30d68ed500a9eddb1fb8371163dff
ANRAx <ramdassaj@gmail.com>
2022-04-05 08:37:46 -0400

    update landing page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import { Stack } from "@mui/material";
 import Header from "../components/Header";
 import CharacterCard from "../components/CharacterCard";
@@ -9,40 +9,32 @@ import {
 } from "../helpers/functions";
 import "./Landing.css";
 
-class Landing extends React.Component {
-  state = {
-    randomCharacterData: [],
-  };
+const Landing = ({ characters }) => {
+  const charactersData = characters;
 
-  componentDidMount() {
-    const { characters } = this.props;
+  const [randomizedCharacterData, setRandomizedCharacterData] = useState([]);
 
-    this.setState({
-      randomCharacterData: randomizeArr(characters),
-    });
-  }
-
-  render() {
-    const { randomCharacterData } = this.state;
-
-    return (
-      <div className="Landing">
-        <div className="landing-header-container">
-          <Header />
-        </div>
-        <Stack direction="row" spacing={3} justifyContent="center">
-          {randomCharacterData?.slice(0, 4).map((character) => (
-            <CharacterCard key={character.id} character={character} />
-          ))}
-        </Stack>
-        <div className="landing-table-container">
-          <TrackTable
-            rows={createRandomCharactersDataRows(randomCharacterData)}
-          />
-        </div>
+  useEffect(() => {
+    setRandomizedCharacterData(randomizeArr(charactersData));
+  }, [charactersData]);
+  
+  return (
+    <div className="Landing">
+      <div className="landing-header-container">
+        <Header />
       </div>
-    );
-  }
+      <Stack direction="row" spacing={3} justifyContent="center">
+        {randomizedCharacterData?.slice(0, 4).map((character) => (
+          <CharacterCard key={character.id} character={character} />
+        ))}
+      </Stack>
+      <div className="landing-table-container">
+        <TrackTable
+          rows={createRandomCharactersDataRows(randomizedCharacterData)}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Landing;
